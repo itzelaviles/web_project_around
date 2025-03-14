f3fc793fd5cb2fd36ca13b39fbd4590498fc6e71
Itzel Aviles <itavgo8@gmail.com>
2025-03-14 16:15:44 -0700

    feat: agregar y eliminar cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,51 +1,133 @@
-let profile = document.querySelector('.profile');
-let popup = document.querySelector('.pop-up');
-let gallery = document.querySelector('.gallery');
+// Profile
+const profile = document.querySelector(".profile");
+const profileName = profile.querySelector(".profile__name");
+const profileDescription = profile.querySelector(".profile__description");
+const profileBtnEdit = profile.querySelector(".profile__btn_edit-info");
+const profileBtnAddPost = profile.querySelector(".profile__btn_add-post");
+// Gallery
+const gallery = document.querySelector(".gallery");
+const galleryBtnDelete = gallery.querySelector(".gallery-card__btn_trash");
+// Edit profile popup
+const editProfilePopup = document.querySelector("#edit-profile-popup");
+const editProfileName = editProfilePopup.querySelector("#popup-name");
+const editProfileDescription =
+  editProfilePopup.querySelector("#popup-about-me");
+const editProfileBtnSave = editProfilePopup.querySelector(".pop-up__btn_save");
+const editProfileBtnClose =
+  editProfilePopup.querySelector(".pop-up__btn_close");
+const editProfileForm = editProfilePopup.querySelector(".pop-up__form");
+// newPlace popup
+const newPlacePopup = document.querySelector("#new-place-popup");
+const newPlaceTitle = newPlacePopup.querySelector("#popup-place-title");
+const newPlaceLink = newPlacePopup.querySelector("#popup-place-img");
+const newPlaceBtnAdd = newPlacePopup.querySelector(".pop-up__btn_save");
+const newPlaceBtnClose = newPlacePopup.querySelector(".pop-up__btn_close");
+const newPlaceForm = newPlacePopup.querySelector(".pop-up__form");
 
-let profileName = profile.querySelector(".profile__name");
-let profileDescription = profile.querySelector(".profile__description");
-let popupName = popup.querySelector('#popup-name');
-let popupDescription = popup.querySelector('#popup-about-me');
-let btnSave = popup.querySelector('.pop-up__btn_save');
-let btnEdit = profile.querySelector('.profile__btn_edit-info');
-let btnClose = popup.querySelector('.pop-up__btn_close');
-let popupForm = popup.querySelector('.pop-up__form');
+const initialCards = [
+  {
+    title: "Chicago Botanic Garden",
+    imageUrl: "images/chicago-botanic-garden.jpg"
+  },
+  {
+    title: "Keukenhof",
+    imageUrl: "images/keukenhof.jpg"
+  },
+  {
+    title: "Lavender Fields",
+    imageUrl: "images/lavender-fields.jpg"
+  },
+  {
+    title: "Skagit Valley",
+    imageUrl: "images/skagit-valley.jpg"
+  },
+  {
+    title: "Ueno Park Tokyo",
+    imageUrl: "images/ueno-park-tokyo.jpg"
+  },
+  {
+    title: "Water Lily Lagoon",
+    imageUrl: "images/water-lily-lagoon.jpg"
+  }
+];
 
-let btnLikes = gallery.querySelectorAll('.gallery-card__btn_like');
-
-
-btnSave.addEventListener('click', handleProfileFormSubmit);
-btnEdit.addEventListener('click', togglePopup);
-btnClose.addEventListener('click', togglePopup);
-popupForm.addEventListener('input', validateForm);
-
-
-btnLikes.forEach((btnLike) => {
-  btnLike.addEventListener('click', () => {
-    toggleLike(btnLike); 
-  });
+// Events
+renderInitialCards();
+editProfileBtnSave.addEventListener("click", handleProfileFormSubmit);
+profileBtnEdit.addEventListener("click", toggleEditProfilePopup);
+editProfileBtnClose.addEventListener("click", toggleEditProfilePopup);
+editProfileForm.addEventListener("input", validateForm);
+profileBtnAddPost.addEventListener("click", toggleNewPlacePopup);
+newPlaceBtnClose.addEventListener("click", toggleNewPlacePopup);
+newPlaceBtnAdd.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  const data = {
+    title: newPlaceTitle.value,
+    imageUrl: newPlaceLink.value,
+  };
+  const card = createCard(data);
+  gallery.prepend(card);
 });
+
+// Functions
+function createCard(data) {
+  const cardTemplate = document.querySelector("#gallery-card-template").content;
+  const cardElement = cardTemplate
+    .querySelector(".gallery-card")
+    .cloneNode(true);
+  cardElement.querySelector(".gallery-card__title").textContent = data.title;
+  const cardImage = cardElement.querySelector(".gallery-card__image");
+  cardImage.src = data.imageUrl;
+  cardImage.alt = data.title;
+
+  const trashButton = cardElement.querySelector(".gallery-card__btn_trash");
+  trashButton.addEventListener("click", () => deleteCard(cardElement));
+
+  const likeBtn = cardElement.querySelector(".gallery-card__btn_like");
+  likeBtn.addEventListener("click", () => {
+    toggleLike(likeBtn);
+  });
+
+  toggleNewPlacePopup();
+  return cardElement;
+}
+
+function deleteCard(card) {
+  card.remove();
+}
+
+function renderInitialCards(){
+  initialCards.forEach((card) => {
+    const cardElement = createCard(card);
+    gallery.prepend(cardElement);
+  });
+}
 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
-  profileName.textContent = popupName.value;
-  profileDescription.textContent = popupDescription.value;
-  togglePopup();
+  profileName.textContent = editProfileName.value;
+  profileDescription.textContent = editProfileDescription.value;
+  toggleEditProfilePopup();
 }
 
-function togglePopup() {
-  popupName.value = profileName.textContent;
-  popupDescription.value= profileDescription.textContent;
-  popup.classList.toggle('pop-up_opened');
+function toggleEditProfilePopup() {
+  editProfileName.value = profileName.textContent;
+  editProfileDescription.value = profileDescription.textContent;
+  editProfilePopup.classList.toggle("pop-up_opened");
+  validateForm();
+}
+
+function toggleNewPlacePopup() {
+  newPlacePopup.classList.toggle("pop-up_opened");
   validateForm();
 }
 
 function validateForm() {
-  if (popupName.value.trim() && popupDescription.value.trim()) {
-    btnSave.disabled = false;
-  } else btnSave.disabled = true;
+  if (editProfileName.value.trim() && editProfileDescription.value.trim()) {
+    editProfileBtnSave.disabled = false;
+  } else editProfileBtnSave.disabled = true;
 }
 
-function toggleLike(btnLike) {
-  btnLike.classList.toggle('gallery-card__btn_like_active');
+function toggleLike(likeBtn) {
+  likeBtn.classList.toggle("gallery-card__btn_like_active");
 }
